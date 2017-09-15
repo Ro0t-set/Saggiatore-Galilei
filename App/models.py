579ab42d00915f5b2f19af6ca87e40e67b00cbd0
Ro0t777 <Tommaso@Ro0t-2.local>
2017-09-15 14:19:19 +0200

    db model 0.1
    essenziale

--- App/models.py
+++ App/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
 
-# Create your models here.
+class Categoria(models.Model):
+    categorie = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.categorie
+
+class Articolo(models.Model):
+    author = models.ForeignKey('auth.User')
+    categorie = models.ForeignKey('Categoria')
+    titolo = models.CharField(max_length=100)
+    text = models.TextField()
+
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titolo
